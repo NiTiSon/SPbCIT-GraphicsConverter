31614a42a35c70cf88e0aa0ad9d39d45d5f91d18
NiTiSon <NiTiSon@users.noreply.github.com>
2023-03-14 20:18:29 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+
+namespace SPbCIT.NiTiSon.GraphicsConverter;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+	protected override void OnLoadCompleted(NavigationEventArgs e)
+	{
+		base.OnLoadCompleted(e);
+	}
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using SharpVectors.Converters;
+using SharpVectors.Renderers.Wpf;
+using System.Globalization;
+using System;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SPbCIT.NiTiSon.GraphicsConverter;
+
+public partial class MainWindow : Window
+{
+	private WpfDrawingSettings settings;
+	private FileSvgReader reader;
+	private ImageSvgConverter converter;
+	private string? imagePath;
+
+	public MainWindow()
+	{
+		InitializeComponent();
+		settings = new();
+		reader = new(settings)
+		{
+			SaveXaml = false,
+			SaveZaml = false,
+		};
+		converter = new(settings)
+		{
+			EncoderType = ImageEncoderType.PngBitmap,
+		};
+	}
+
+	private void ConvertButtonClick(object sender, RoutedEventArgs e)
+	{
+		if (imagePath is null)
+			return;
+
+		SaveFileDialog saveFileDialog = new()
+		{
+			Filter = "Portable Network Graphics|*.png"
+		};
+
+		if (saveFileDialog.ShowDialog() == true)
+		{
+			if (!converter.Convert(imagePath, saveFileDialog.FileName))
+			{
+				MessageBox.Show("Unable to convert vector image", "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+			}
+			else
+			{
+				MessageBox.Show($"Image saved at path: {saveFileDialog.FileName}", "Success", MessageBoxButton.OK, MessageBoxImage.None);
+			}
+		}
+	}
+	private void SelectButtonClick(object sender, RoutedEventArgs e)
+	{
+		OpenFileDialog dialog = new()
+		{
+			Filter = "SVG files|*.svg",
+		};
+
+		if (dialog.ShowDialog() == true)
+		{
+			DrawingGroup dg = reader.Read(dialog.FileName);
+
+			canvas.Background = Brushes.Transparent;
+
+			canvas.UnloadDiagrams();
+
+			if (dg != null)
+			{
+				zoomPan.ZoomTo(dg.Bounds);
+				canvas.RenderSize = new(400, 400);
+				canvas.RenderDiagrams(dg);
+
+				imagePath = dialog.FileName;
+			}
+		}
+	}
+}
